73471a43bf5ca8bf54f612a3a10f7d85351c94f6
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2020-12-01 09:33:02 -0500

    Initialize AdventOfCode2020

---
+++ day_template.rb
@@ -0,0 +1,10 @@
+require_relative 'input'
+
+day = __FILE__[/\d+/].to_i(10)
+input = Input.for_day(day, 2020)
+
+puts "solving day #{day} from input"
+
+input.each_line(chomp: true) do |line|
+  puts input
+end
